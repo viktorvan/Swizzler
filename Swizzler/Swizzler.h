80275fe8d2452e855390db71a7555bf863cc3749
Viktor Andersson <v.andersson@me.com>
2015-02-13 13:33:09 +0100

    Initial Commit

---
+++ Swizzler/Swizzler.h
@@ -0,0 +1,13 @@
+//
+//  Swizzler.h
+//  Swizzler
+//
+//  Created by Viktor Andersson on 13/02/15.
+//  Copyright (c) 2015 Viktor Andersson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Swizzler : NSObject
+
+@end
---
+++ Swizzler/Swizzler.m
@@ -0,0 +1,13 @@
+//
+//  Swizzler.m
+//  Swizzler
+//
+//  Created by Viktor Andersson on 13/02/15.
+//  Copyright (c) 2015 Viktor Andersson. All rights reserved.
+//
+
+#import "Swizzler.h"
+
+@implementation Swizzler
+
+@end
