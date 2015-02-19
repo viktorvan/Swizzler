4c51c7e4704d554f869f14386cf1bc54e34c04a5
Viktor Andersson <viktorvan@users.noreply.github.com>
2015-02-19 12:30:23 +0100

    Add HeaderDoc comments in Swizzler.h.

--- Swizzler/Swizzler.h
+++ Swizzler/Swizzler.h
@@ -8,16 +8,42 @@
 
 #import <Foundation/Foundation.h>
 
+/*! @typedef ActionBlock
+ * @brief An action.
+ */
 typedef void (^ActionBlock)(void);
 
+/*!
+ * @class Swizzler
+ * @discussion Swizzles class methods.
+ */
 @interface Swizzler : NSObject
 
 - (instancetype) init __attribute__((unavailable("init not available")));
+
+/*!
+ * @brief Initializes a Swizzler with the following parameters.
+ * @param theSelector Selector to swizzle.
+ * @param theTargetClass The class on which the selector should be replaced.
+ * @param theSwizzleClass The class with the method that will be replacing the target class method.
+ */
 - (instancetype) initWithSelector:(SEL)theSelector
                             class:(Class)theTargetClass
                             class:(Class)theSwizzleClass;
 
+/*!
+ * @discussion The ActionBlock anAction will be called while the Swizzlers methods are swizzled. 
+ * When the block returns the methods will be deSwizzled.
+ * @param anAction An ActionBlock that will be called while the methods are swizzled.
+ * @code [swizzler doWhileSwizzled: ^{
+   // Do something useful while the methods are swizzled.
+ }];
+ */
 - (void) doWhileSwizzled:(ActionBlock) anAction;
+
+/*! @discussion De-swizzle the methods. After sending this message to the Swizzler the methods will be returned to
+ * their original unswizzled state.
+ */
 - (void) deSwizzle;
 
 @end
